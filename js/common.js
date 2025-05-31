811fc896767fe8e031392429a34634be31920bb8
Davide Lancini <davidelenc@gmail.com>
2025-05-31 12:20:25 +0200

    structure fixes

--- js/common.js
+++ js/common.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
  if (header) {
-  header.innerHTML = `<div class="container"><h1><a href="/index.html" id="logo">Dev.</a></h1><nav id="nav"><ul>
-<li class><a href="/articles/con_indice.html">Con Indice</a></li>
-<li class><a href="/articles/senza_indice.html">Senza Indice</a></li>
+  header.innerHTML = `<div class="container"><h1><a href="index.html" id="logo">Dev.</a></h1><nav id="nav"><ul>
+<li class><a href="con_indice.html">Con Indice</a></li>
+<li class><a href="senza_indice.html">Senza Indice</a></li>
 </ul></nav></div>`;
  }
  if (footer) {
